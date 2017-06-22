a0bba94aca0cd3ad235f282623259f056733e802
Minh Nguyen <minh18.nguyen@gmail.com>
2017-06-22 22:44:03 +0200

    Ajout des statistiques

--- HTML/Vues/README[1906].txt
+++
@@ -1,5 +0,0 @@
-Il faut refaire les reque^tes sql pour la visualisation des cartes pour le client.
-
-Il faut aussi trouver comment se connecter au serveur tuxa via le PDO
-
-Il faut faire les cas : serveurs / managers
--- HTML/Vues/Statistiques.php
+++ HTML/Vues/Statistiques.php
@@ -8,109 +8,139 @@
               <h3>Classement des plats les plus commandes<strong></h3>
                       <?php
                           /** Connexion **/
-                          $connexion = new PDO('pgsql:host=tuxa.sme.utc;port=5432;dbname=nf17p122', 'TaeORGh5');
+                          $dbconn = pg_connect("host=tuxa.sme.utc dbname=dbnf17p122 user=nf17p122 password=TaeORGh5") or die('Connexion impossible : ' . pg_last_error());
                           /** Préparation et exécution de la requête **/
-                          $sql = "SELECT P.NomPlat, sum(QE.Quantite)
-					FROM Plat P, Menu M, ContenuMenu CM, QuantiteElem QE
-					WHERE (QE.IDElement=P.ID
-					AND CM.IDPlat=P.ID
-					AND M.ID=CM.IDMenu)
-					OR (QE.IDElement = M.ID
-					AND P.ID = CM.IDPlat
-					AND CM.IDMenu = M.ID)
-					GROUP BY P.NomPlat
-					ORDER BY SUM(QE.Quantite) DESC";
-                          $resultset = $connexion->prepare($sql);
-                          $resultset->execute();
+                          $query = "SELECT e1.NomPlat, sum(e1.Quantite) AS QuantiteVendue
+                                        FROM (
+                                        	SELECT P.NomPlat, sum(QE.Quantite)AS Quantite
+                                        	FROM Plat P, QuantiteElem QE
+                                        	WHERE QE.IDElement=P.ID
+                                        	GROUP BY P.NomPlat
 
+                                        	UNION (
+
+                                        	SELECT P.NomPlat, sum(QE.Quantite) AS Quantite
+                                        	FROM Plat P, Menu M, ContenuMenu CM, QuantiteElem QE
+                                        	WHERE QE.IDElement = M.ID
+                                        	AND P.ID = CM.IDPlat
+                                        	AND CM.IDMenu = M.ID
+                                        	GROUP BY P.NomPlat	 )
+                                        ) AS e1
+                                        GROUP BY e1.NomPlat
+                                        ORDER BY SUM(e1.Quantite) DESC";
+                          $result = pg_query($query) or die('Échec de la requête : ' . pg_last_error());
+                          echo "<table>\n";
+                          			echo "<tr>
+                          			       <th>Nom Plat</th>
+                          			       <th>Quantite d elements</th>
+                          			   </tr>";
                           /** Traitement du résultat **/
-                          echo "Plats les plus commandés";
-                          while ($row = $resultset->fetch(PDO::FETCH_ASSOC)) {
-                          echo "'Nom du plat: '" . $row[P.NomPlat] . "'Quantitee de plat commandé: '" . $row['quantite']  ;
-                          }
+
+                          while ($row = pg_fetch_array($result, null, PGSQL_ASSOC)) {
+                      			echo "\t<tr>\n";
+                      			foreach ($row as $col_value) {
+                      			        echo "\t\t<td>$col_value</td>\n";
+                      			    }
+                      			    echo "\t</tr>\n";
+                      			}
+                      		echo "</table>\n";
+                          echo  $row['e1.NomPlat'] .  $row['QuantiteVendue']  ;
                           /** Déconnexion **/
-                          $connexion=null;
+                          pg_free_result($result);
+      			  pg_close($dbconn);
                       ?>
                 <h3> Les plats a base de viande sont-ils plus commandes que les plats à base de poisson?<strong> </h3>
                       <?php
                           /** Connexion **/
-                          $connexion = new PDO('pgsql:host=tuxa.sme.utc;port=5432;dbname=nf17p122', 'TaeORGh5');
-
+$dbconn = pg_connect("host=tuxa.sme.utc dbname=dbnf17p122 user=nf17p122 password=TaeORGh5") or die('Connexion impossible : ' . pg_last_error());
                           /** Préparation et exécution de la requête **/
-                          $sql = "SELECT P.Categorie, COUNT(P.Categorie) AS Quantite  /**fonctionne?**/
-                          FROM Plat P, Menu M, ContenuMenu CM, QuantiteElem QE
-                          WHERE (QE.IDElement=P.ID
-                          AND CM.IDPlat=P.ID
-                          AND M.ID=CM.IDMenu
-                          AND(P.Categorie='poisson'OR P.Categorie='viande'))
-                          OR (QE.IDElement = M.ID
-                          AND P.ID = CM.IDPlat
-                          AND CM.IDMenu = M.ID
-                          AND(P.Categorie='poisson'OR P.Categorie='viande'))
-                          GROUP BY P.Categorie
-                          ORDER BY SUM(QE.Quantite) DESC";
-                          $resultset = $connexion->prepare($sql);
-                          $resultset->execute();
+                          $query = "SELECT e1.Categorie, SUM(e1.Quantite) AS QuantiteVendue
+                                        FROM (
+                                        	SELECT P.Categorie, sum(QE.Quantite)AS Quantite
+                                        	FROM Plat P, QuantiteElem QE
+                                        	WHERE QE.IDElement=P.ID
+                                        	GROUP BY P.Categorie
+                                        UNION (
+                                        	SELECT P.Categorie, sum(QE.Quantite) AS Quantite
+                                        	FROM Plat P, Menu M, ContenuMenu CM, QuantiteElem QE
+                                        	WHERE QE.IDElement = M.ID
+                                        	AND P.ID = CM.IDPlat
+                                        	AND CM.IDMenu = M.ID
+                                        	GROUP BY P.Categorie	 )
+                                        ) AS e1
+                                        WHERE e1.Categorie='Poisson'OR e1.Categorie='Viande'
+                                        GROUP BY e1.Categorie
+                                        ORDER BY SUM(e1.Quantite) DESC";
+                        $result = pg_query($query) or die('Échec de la requête : ' . pg_last_error());
 
+                        echo "<table>\n";
+                        			echo "<tr>
+                        			       <th>Categorie</th>
+                        			       <th>Quantite</th>
+                        			   </tr>";
                           /** Traitement du résultat **/
-                          echo "<Plats>";
-                          while ($row = $resultset->fetch(PDO::FETCH_ASSOC)) {
-                          echo  "'Categorie: '" . $row['P.Categorie'] . "'Quantite:  '" . $row['Quantite'] ;
-                          }
+                          while ($row = pg_fetch_array($result, null, PGSQL_ASSOC)) {
+                              			echo "\t<tr>\n";
+                              			foreach ($row as $col_value) {
+                              			        echo "\t\t<td>$col_value</td>\n";
+                              			    }
+                              			    echo "\t</tr>\n";
+                              			}
+                              		echo "</table>\n";
+                          echo   $row['e1.Categorie'] . $row['QuantiteVendue'] ;
+
                           /** Déconnexion **/
-                          $connexion=null;
+                          pg_free_result($result);
+                          pg_close($dbconn);
                       ?>
-                      <h3> Il y a t il plus de plats commandes a base de viande qu'a base de poisson? <strong> </h3>
-                        <?php
-                            /** Connexion **/
-                            $connexion = new PDO('pgsql:host=tuxa.sme.utc;port=5432;dbname=nf17p122', 'TaeORGh5');
 
-                            /** Préparation et exécution de la requête **/
-                            $sql = "SELECT P.Categorie, SUM(QE.Quantite)
-					FROM Plat P, Menu M, ContenuMenu CM, QuantiteElem QE
-					WHERE ((QE.IDElement=P.ID
-					AND CM.IDPlat=P.ID
-					AND M.ID=CM.IDMenu)
-					OR (QE.IDElement = M.ID
-					AND P.ID = CM.IDPlat
-					AND CM.IDMenu = M.ID))
-					AND(P.Categorie='Poisson'OR P.Categorie='Viande')
-					GROUP BY P.Categorie
-					ORDER BY SUM(QE.Quantite) DESC";
-                            $resultset = $connexion->prepare($sql);
-                            $resultset->execute();
-
-                            /** Traitement du résultat **/
-                            echo "<Resultat>";
-                            while ($row = $resultset->fetch(PDO::FETCH_ASSOC)) {
-                            echo  "'Categorie: '" . $row['P.Categorie'] . "'Quantite:  '" . $row['quantite'] ;
-                            }
-                            /** Déconnexion **/
-                            $connexion=null;
-                        ?>
                         <h3> Somme moyenne depensee pour les desserts pour chaque commande <strong> </h3>
                           <?php
                               /** Connexion **/
-                              $connexion = new PDO('pgsql:host=tuxa.sme.utc;port=5432;dbname=nf17p122', 'TaeORGh5');
-
+$dbconn = pg_connect("host=tuxa.sme.utc dbname=dbnf17p122 user=nf17p122 password=TaeORGh5") or die('Connexion impossible : ' . pg_last_error());
                               /** Préparation et exécution de la requête **/
-                              $sql = "SELECT P.t, AVG(PR.Prix*QE.Quantite) 
-						AS DepenseMoyenneDessert
-						FROM Plat P, Prix Pr, QuantiteElem QE
-						WHERE P.t='Dessert'
-						AND QE.IDElement=P.ID
-						AND Pr.IDElement=P.ID
-						GROUP BY P.t";
-                              $resultset = $connexion->prepare($sql);
-                              $resultset->execute();
+                              $query = "SELECT e1.t, AVG(e1.PrixCommandeMoyen) AS DepenseMoyenneDessert
+                                          FROM (
+                                          	SELECT P.t, avg(QE.Quantite*Pr.Prix)AS PrixCommandeMoyen
+                                          	FROM Plat P, QuantiteElem QE, Prix Pr
+                                          	WHERE QE.IDElement=P.ID
+                                          	AND Pr.IDElement=P.ID
+                                          	GROUP BY P.t
 
+                                          UNION (
+
+                                          	SELECT P.t, avg(QE.Quantite*Pr.Prix) AS PrixCommandeMoyen
+                                          	FROM Plat P, Menu M, ContenuMenu CM, QuantiteElem QE, Prix Pr
+                                          	WHERE QE.IDElement = M.ID
+                                          	AND P.ID = CM.IDPlat
+                                          	AND CM.IDMenu = M.ID
+                                          	AND Pr.IDElement=P.ID
+                                          	GROUP BY P.t	 )
+                                          ) AS e1
+                                          WHERE e1.t='Dessert'
+                                          GROUP BY e1.t
+                                          ";
+                             $result = pg_query($query) or die('Échec de la requête : ' . pg_last_error());
+                             echo "<table>\n";
+                             			echo "<tr>
+                             			       <th>Dessert</th>
+                             			       <th>Depense Moyenne</th>
+                             			   </tr>";
                               /** Traitement du résultat **/
-                              echo "<Resultat>";
-                              while ($row = $resultset->fetch(PDO::FETCH_ASSOC)) {
-                              echo  "'Dessert: '" . $row['P.t'] . "'Quantite:  '" . "'Somme moyenne des desserts: '" . $row['moyenne'] ;
+                              while ($row = pg_fetch_array($result, null, PGSQL_ASSOC)) {
+                        			echo "\t<tr>\n";
+                        			foreach ($row as $col_value) {
+                        			        echo "\t\t<td>$col_value</td>\n";
+                        			    }
+                        			    echo "\t</tr>\n";
+                        			}
+                        		echo "</table>\n";
+
+                              echo  $row['e1.t'] . $row['DepenseMoyenneDessert'] ;
                               }
                               /** Déconnexion **/
-                              $connexion=null;
+                              pg_free_result($result);
+    			                    pg_close($dbconn);
                           ?>
           </div>
         </body>
